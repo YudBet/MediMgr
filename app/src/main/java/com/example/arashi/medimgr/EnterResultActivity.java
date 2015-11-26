5b01589cfc4e41c13890dc35e87e3b1439466080
YudBet <lol0lllmist@gmail.com>
2015-11-27 07:03:52 +0800

    Get data in Parse.com

--- app/src/main/java/com/example/arashi/medimgr/EnterResultActivity.java
+++ app/src/main/java/com/example/arashi/medimgr/EnterResultActivity.java
@@ -1,84 +1,127 @@
 package com.example.arashi.medimgr;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.io.InputStream;
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 
 public class EnterResultActivity extends ActionBarActivity {
 
-    private String from = "";
-    private String num = "";
+    private static final String[] PARSE_CLASSES = {
+            "drug_dosaage", "drug_ingredient", "drug_apprence"};
+    public static final int DRUG_DOSAGE = 0;
+    public static final int DRUG_INGREDIENT = 1;
+    public static final int DRUG_APPRENCE = 2;
+
+    private String drug_id,
+            ch_name, drug_dosage, indications,  // from class drug_dosage
+            drug_ingredient,                    // from class drug_ingredient
+            color, shape, apprence_url;         // from class drug_apprence
+
+    TextView text;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_result);
 
-        initFromAndNum(savedInstanceState);
+        drug_id = getDrugIdFromBundle(savedInstanceState);
 
-        String drug_id = from + "字第" + num + "號";
+        initDrugInfo();
 
-
-        new DownloadImageTask((ImageView) findViewById(R.id.medi_img))
-                .execute("http://www.fda.gov.tw/MLMS/ShowFile.aspx?LicId=01023889&Seq=133&Type=10");
-
-
-        TextView text = (TextView)findViewById(R.id.text);
-        text.setText(drug_id);
-
-
-
+        text = (TextView)findViewById(R.id.text);
     }
 
-    public void initFromAndNum(Bundle savedInstanceState) {
+    public String getDrugIdFromBundle(Bundle savedInstanceState) {
+        String drug_id;
+        String from = "";
+        String num = "";
+
         if (savedInstanceState == null) {
             Bundle bundle = getIntent().getExtras();
             if (bundle != null) {
-                this.from = bundle.getString("FROM");
-                this.num = bundle.getString("NUM");
+                from = bundle.getString("FROM");
+                num = bundle.getString("NUM");
             }
         }
         else {
-            this.from = (String)savedInstanceState.getSerializable("FROM");
-            this.num = (String)savedInstanceState.getSerializable("NUM");
+            from = (String)savedInstanceState.getSerializable("FROM");
+            num = (String)savedInstanceState.getSerializable("NUM");
         }
+
+        drug_id = from + "字第" + num + "號";
+        return drug_id;
     }
 
 
-    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
-        ImageView bmImage;
+    public void initDrugInfo() {
+        initDrugDosage("drug_id", drug_id); // init ch_name, drug_dosage, indications
+        initDrugIngredient("drug_id", drug_id); // init drug_ingredient
+        initDrugApprence("drug_id", drug_id); // init color, shape, apprence_url
+    }
 
-        public DownloadImageTask(ImageView bmImage) {
-            this.bmImage = bmImage;
-        }
-
-        protected Bitmap doInBackground(String... urls) {
-            String urldisplay = urls[0];
-            Bitmap mIcon11 = null;
-            try {
-                InputStream in = new java.net.URL(urldisplay).openStream();
-                mIcon11 = BitmapFactory.decodeStream(in);
-            } catch (Exception e) {
-                Log.e("Error", e.getMessage());
-                e.printStackTrace();
+    public void initDrugDosage(String where, String equalTo) {
+        final ParseQuery<ParseObject> query = ParseQuery.getQuery(PARSE_CLASSES[DRUG_DOSAGE]);
+        query.whereEqualTo(where, equalTo);
+        query.getFirstInBackground(new GetCallback<ParseObject>() {
+            @Override
+            public void done(ParseObject object, ParseException e) {
+                if (e == null) {
+                    ch_name = object.getString("ch_name");
+                    drug_dosage = object.getString("drug_dosage");
+                    indications = object.getString("indications");
+                } else {
+                    Log.d("drug_dosage", "Error: " + e.getMessage());
+                }
             }
-            return mIcon11;
-        }
-
-        protected void onPostExecute(Bitmap result) {
-            bmImage.setImageBitmap(result);
-        }
+        });
     }
+
+    public void initDrugIngredient(String where, String equalTo) {
+        final ParseQuery<ParseObject> query = ParseQuery.getQuery(PARSE_CLASSES[DRUG_INGREDIENT]);
+        query.whereEqualTo(where, equalTo);
+        query.getFirstInBackground(new GetCallback<ParseObject>() {
+            @Override
+            public void done(ParseObject object, ParseException e) {
+                if (e == null) {
+                    drug_ingredient = object.getString("drug_ingredient");
+                } else {
+                    Log.d("drug_ingredient", "Error: " + e.getMessage());
+                }
+            }
+        });
+    }
+
+    public void initDrugApprence(String where, String equalTo) {
+        final ParseQuery<ParseObject> query = ParseQuery.getQuery(PARSE_CLASSES[DRUG_APPRENCE]);
+        query.whereEqualTo(where, equalTo);
+        query.getFirstInBackground(new GetCallback<ParseObject>() {
+            @Override
+            public void done(ParseObject object, ParseException e) {
+                if (e == null) {
+                    color = object.getString("color");
+                    shape = object.getString("shape");
+                    apprence_url = object.getString("apprence_url");
+                } else {
+                    Log.d("drug_apprence", "Error: " + e.getMessage());
+                }
+            }
+        });
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
